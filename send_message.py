e363925081a5cb31becf9b44441e0e7e73c250c2
karan-maheshwari <karanmaheshwari24@gmail.com>
2019-04-04 19:42:50 -0700

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -2,7 +2,8 @@ from twilio.rest import Client
 
 
 def send_message(number, message):
-    
+    account_sid = 'insert_from_instructions_file'
+    auth_token = 'insert_from_instructions_file'
     client = Client(account_sid, auth_token)
 
     sent_message = client.messages \
